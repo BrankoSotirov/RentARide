a208a167ad2093961542232e2d76fc23323c6027
brank <brankosotirov@gmail.com>
2024-10-30 09:35:56 +0200

    removing privacy, moving the menu to a different viewcomponent

---
+++ Components/MainMenuComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentARide.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+         
+        }
+
+    }
+}
